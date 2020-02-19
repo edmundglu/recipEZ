8303abc47fc08fa7beb648793c90dd6f9d7d2829
edmundglu <edmundlu@Edmunds-MacBook-Pro.local>
2020-02-19 15:32:59 -0800

    Updates

---
+++ precache-manifest.241be38856766217273ba5da56f3a40d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f591b5174e71c2303197abd13d9d6e",
+    "url": "/recipEZ/index.html"
+  },
+  {
+    "revision": "d862cab3b55bbfd8c1dc",
+    "url": "/recipEZ/static/css/main.c15d6763.chunk.css"
+  },
+  {
+    "revision": "26dfd1fb1be323a0bbd1",
+    "url": "/recipEZ/static/js/2.e90b0220.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/recipEZ/static/js/2.e90b0220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d862cab3b55bbfd8c1dc",
+    "url": "/recipEZ/static/js/main.74af3a30.chunk.js"
+  },
+  {
+    "revision": "fe3470ac2638b5d3122b",
+    "url": "/recipEZ/static/js/runtime-main.50817c02.js"
+  }
+]);
